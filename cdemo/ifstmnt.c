6b0e60f441481eb53ecc26101f9732d73db91834
Sophia Yu <yusophia2011@gmail.com>
2019-05-22 09:00:45 -0700

    Selection_COMPLETE

--- cdemo/ifstmnt.c
+++ cdemo/ifstmnt.c
@@ -3,15 +3,42 @@
 int main()
 {
   int a = 0;
+  int b = 15;
 
+	printf("A: %d   B: %d\n",a,b);
   // if statement to test is a is equal to 0
-  if (a == 0)
+  if (a == b)
   {
-    printf("a is 0\n");
+    printf("a is equal to b\n");
   }
-  else
+
+  if (a != 0)
   {
-    printf("a is not 0\n");
+    printf("a is not equal to b\n");
   }
-    
+
+  if (a > b)
+  {
+    printf("a is greater than b\n");
+  }
+
+  if (a >= 0)
+  {
+    printf("a is greater than equal to b\n");
+  }
+
+  if (a == 0 && b ==0)
+  {
+    printf("a is equal to 0 AND b is equal to 0\n");
+  }
+
+  if (a == 0 || b == 0)
+  {
+    printf("a is equal to 0 OR b is equal to 0\n");
+  }
+
+  if (!(a == 0))
+  {
+    printf("a is not equal to 0\n");
+  }
 }
