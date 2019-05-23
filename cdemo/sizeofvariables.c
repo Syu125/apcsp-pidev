b9653720bd21a237639e74b388e178f321e6d032
Sophia Yu <yusophia2011@gmail.com>
2019-05-23 08:25:41 -0700

    SizeOf_COMPLETE

--- cdemo/sizeofvariables.c
+++ cdemo/sizeofvariables.c
@@ -3,14 +3,18 @@
 int main()
 {
   int a = 545;
-  char c = "s";
+  char k = 's';
   float f = 12.345;
   double d = 134.624;
+  unsigned int b = 450;
+  signed int e = 15;
 
   // print value and size of an int variable
   printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
-  printf("char c value: %d and size: %d bytes\n", c, sizeof(c));
-  printf("float f value: %d and size: %d bytes\n", f, sizeof(f));
+  printf("char k value: %c and size: %d bytes\n", k, sizeof(k));
+  printf("float f value: %f and size: %d bytes\n", f, sizeof(f));
   printf("double d value: %d and size: %d bytes\n", d, sizeof(d));
+  printf("int b value: %d and size: %d bytes\n", b, sizeof(b));
+  printf("int e value: %f and size: %d bytes\n", e, sizeof(e));
 }
 
