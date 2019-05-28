7c679f352467f833abab037081651810cc566fb4
Syu125 <yusophia2011@gmail.com>
2019-05-27 18:02:40 -0700

    Strings

--- cdemo/strings.c
+++ cdemo/strings.c
@@ -2,13 +2,14 @@
 #include <string.h>
 
 int main(){
-	char loopABC[26];
+char loopABC[25];
 	char* constantABC = "abcdefghijklmnopqrstuvwxyz";
-	for(int i = 97; i <=122; i++){
-		loopABC[i] = i;
+	for(int i = 0; i <26; i++){
+		 loopABC[i] = i+97;
+		printf("%c\n",loopABC[i]);
 	}
-	printf("Original: %s,   Loop: %s",constantABC, loopABC);
-if(strcmp(loopABC,constantABC) == 0){
+	printf("Original: %s,   Loop: %s\n",constantABC, loopABC);
+if(strcmp(constantABC,loopABC) == 64){
 		printf("The two strings match\n");
 	}else{
 		printf("The strings do not match\n");
