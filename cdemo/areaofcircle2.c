c0794c5081c9d14ef3100f08f2cd196f0f5c5223
Sophia Yu <yusophia2011@gmail.com>
2019-05-31 11:36:01 -0700

    Areaofcircle2_COMPLETE

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 const double pi = 3.14159265897;
@@ -5,21 +6,23 @@ double areaOfCircle(double r){
 double result = r*r*pi;
 return result;
 }
+
 int main(int argc, char* argv[]){
 float l;
 float h;
 char *a = argv[1];
 char *b = argv[2];
-
 l = atof(a);
 h = atof(b);
 int arg = 1;
 while(arg < argc){
-for(int i = l; i <= h; i++){
-int r = i;
+for(double i = l; i <= h; i++){
+double r = i;
 double get = areaOfCircle(r);
-printf("The area of a circle with radius %d is %f\n",r, get);
+printf("Area of %f is %f\n", r, get);
 arg++;
 }
 }
 }
+
+
